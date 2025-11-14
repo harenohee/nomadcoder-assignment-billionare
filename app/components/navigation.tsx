fc1e3d8b57f7deb65736f1589ca13cad07b0c4b9
harenohee <panda4859@gmail.com>
2025-11-14 18:16:55 +0900

    홈화면 작업

---
+++ app/components/navigation.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navigation = () => {
+  return (
+    <div className="p-2">
+      <Link href="/">
+        <Image
+          className="dark:invert"
+          src="/home.svg"
+          alt="logo"
+          width={50}
+          height={50}
+          priority
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Navigation;
